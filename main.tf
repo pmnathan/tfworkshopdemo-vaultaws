99d5a1c45ee86a9f60e1e9488422a1defd153a48
Prakash Manglanathan <pmnathan@gmail.com>
2022-04-14 12:43:57 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "vault" {
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "tfc-role"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+}
+
+resource "aws_vpc" "hashi" {
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "tfc-test-vpc"
+  }
+}
