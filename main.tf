20385813b9f1be6faad8e97f45a41ebb6a690adc
Prakash Manglanathan <pmnathan@gmail.com>
2022-11-17 19:10:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,19 @@
 provider "vault" {
 }
 
-data "vault_aws_access_credentials" "creds" {
-  backend = "aws"
-  role    = "tfc-role"
-}
+#data "vault_aws_access_credentials" "creds" {
+#  backend = "aws"
+#  role    = "tfc-role"
+#}
 
 provider "aws" {
   region = "us-east-1"
-  access_key = data.vault_aws_access_credentials.creds.access_key
-  secret_key = data.vault_aws_access_credentials.creds.secret_key
+  #access_key = data.vault_aws_access_credentials.creds.access_key
+  #secret_key = data.vault_aws_access_credentials.creds.secret_key
 }
 
 resource "aws_s3_bucket" "b" {
   bucket = "com-hashicorp-prakash-demo-2022"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-
-resource "aws_s3_bucket" "b2" {
-  bucket = "com-hashicorp-prakash-demo-2022-april"
 
   tags = {
     Name        = "My bucket"
