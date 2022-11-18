107ca56a9589cfd089bb6fa329bf9507cb06a64a
Prakash Manglanathan <pmnathan@gmail.com>
2022-11-18 10:12:16 -0500

    added vault

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "vault" {
 }
 
-#data "vault_aws_access_credentials" "creds" {
-#  backend = "aws"
-#  role    = "test"
-#}
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "test"
+}
 
 provider "aws" {
   region = "us-east-1"
- # access_key = data.vault_aws_access_credentials.creds.access_key
- # secret_key = data.vault_aws_access_credentials.creds.secret_key
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
 }
 
 resource "aws_s3_bucket" "a" {
